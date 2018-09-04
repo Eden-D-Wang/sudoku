71352d1d9cddaf91aa4927b90a783ff93a29d41a
Eden D Wang <eden.d.wang@pwc.com>
2018-09-04 08:43:05 +0800

    run python code

--- server/index.js
+++ server/index.js
@@ -1,18 +1,31 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const formidable = require('formidable');
+const fs = require('fs');
 const http = require('http');
+const childProcess = require('child_process');
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const runPy = (filename) => new Promise((resolve, reject) => {
+    const process = childProcess.spawn('python', ['../SnapSudoku/sudoku.py', filename]);
+    process.stdout.on('data', (data) => {
+        console.log(data.toString());
+        resolve();
+    });
+
+    process.stderr.on('data', (data) => {
+        console.error(data.toString());
+        reject();
+    });
+});
 
 app.post('/sudoku', function (req, res) {
     const form = new formidable();
-    form.uploadDir = 'sudoku/';
+    form.uploadDir = '../SnapSudoku/';
     form.keepExtensions = true;
 
     form.parse(req, function (err, fields, files) {
-        res.send();
+        runPy(files.file.name).then(() => fs.unlinkSync(files.file.path));
+        res.send(files.file.name);
     });
 });
 
